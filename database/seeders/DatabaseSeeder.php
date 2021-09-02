e8333e6a8445d6efdf9100f325c1d0e85cfbdeab
yinka <holayinkajr@gmail.com>
2021-09-02 04:38:17 +0100

    first files

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        // \App\Models\Tag::factory(10)->create();
+        $tags = \App\Models\Tag::factory(10)->create();
+        
+        \App\Models\User::factory(20)->create()->each(function ($user) use($tags){
+            \App\Models\Listing::factory(rand(1,4))->create([
+                'user_id' => $user->id
+            ])-> each(function($listing) use($tags){
+                $listing->tags()->attach($tags->random(2));
+            });
+        });
+    }
+}
